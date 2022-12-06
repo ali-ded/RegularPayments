a8a2adb15639a9abe83812b4fa69bc9a3808efaa
Petro Pryvarnikov <p.v.pryvarnikov@gmail.com>
2022-12-06 19:18:02 +0200

    Initial commit

---
+++ src/main/java/com/payments/regularpayments/RegularPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.payments.regularpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegularPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegularPaymentsApplication.class, args);
+    }
+
+}
