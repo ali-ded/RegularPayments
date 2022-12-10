c25f1be7c1e833b855f60634749cce9f7ded432e
Petro Pryvarnikov <p.v.pryvarnikov@gmail.com>
2022-12-10 02:06:32 +0200

    Add flyway

---
+++ src/main/resources/db/migration/V1_0__create_tables.sql
@@ -0,0 +1,42 @@
+create table person
+(
+    id              bigserial   primary key,
+    name            varchar(50) not null,
+    surname         varchar(50) not null,
+    patronymic      varchar(50),
+    phone_number    varchar(15) not null unique,
+    inn             bigint      unique check (inn > 0)
+)
+;
+
+create sequence bank_account_seq minvalue 100000000000 maxvalue 999999999999;
+
+create table bank_account
+(
+    id              bigint          primary key default nextval('bank_account_seq'),
+    sum_of_money    numeric(14,2)   not null,
+    person_id       bigint          not null references person on delete cascade,
+    constraint valid_bank_account_range check (id >= 100000000000 and id <= 999999999999)
+)
+;
+
+alter sequence bank_account_seq owned by bank_account.id;
+
+create table payment
+(
+    id                  bigserial       primary key,
+    credit_account      bigint          not null references bank_account on delete cascade,
+    debit_account       bigint          not null references bank_account on delete cascade,
+    transaction_amount  numeric(14,2)   check (transaction_amount > 0),
+    constraint check_for_identical_accounts check (credit_account != debit_account)
+)
+;
+
+create table journal_entry
+(
+    id              bigserial   primary key,
+    payment_id      bigint      not null references payment on delete cascade,
+    payment_result  boolean     not null,
+    payment_date    timestamp   not null default current_timestamp
+)
+;
